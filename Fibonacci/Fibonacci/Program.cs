ed57707ecacd05acbdf3e755eeef50ce74a3ed7a
kdvsolis <32238701+kdvsolis@users.noreply.github.com>
2024-09-11 12:01:18 +0800

    Add files via upload

---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,17 @@
+﻿int limit = 4000000;
+int sum = 0;
+int a = 1;
+int b = 2;
+
+while (b <= limit)
+{
+    if (b % 2 == 0)
+    {
+        sum += b;
+    }
+    int temp = a + b;
+    a = b;
+    b = temp;
+}
+
+Console.WriteLine("The sum of the even-valued terms in the Fibonacci sequence below four million is: " + sum);
---
+++ NaturalNumbers/NaturalNumbers/Program.cs
@@ -0,0 +1,9 @@
+﻿int sum = 0;
+for (int i = 1; i < 1000; i++)
+{
+    if (i % 3 == 0 || i % 5 == 0)
+    {
+        sum += i;
+    }
+}
+Console.WriteLine("The sum of all multiples of 3 or 5 below 1000 is: " + sum);
